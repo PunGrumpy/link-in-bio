66444bb8af01eb706428771094dbc0e12733d20d
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-05-05 22:17:40 +0700

    style: website meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,24 @@ const Layout: React.FC<LayoutProps> = ({ children, title, description }) => {
         <title>{title}</title>
         <meta name="description" content={description} />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="author" content="Noppakorn Kaewsalabnil" />
+        <meta name="keywords" content="Link in Bio, Instagram, Linktree" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="Link in Bio" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://bio.pungrumpy.com" />
+        <meta property="og:image" content="/background.jpg" />
+        <meta property="og:description" content={description} />
+        <meta property="og:site_name" content="Link in Bio" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@wtpp_p114" />
+        <meta name="twitter:creator" content="@wtpp_p114" />
+        <meta name="twitter:title" content="Link in Bio" />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content="/background.jpg" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
