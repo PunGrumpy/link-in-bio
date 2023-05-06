ba97cb81484611400fde97f18c61a3521670a3f7
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-05-06 22:00:51 +0700

    style(hover): scale on hover

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const LinkCard: React.FC<{ href: string; title: string }> = ({
 }) => (
   <li>
     <a
-      className="box block rounded-lg text-white font-mplus font-bold text-center hover:bg-[rgba(255,255,255,0.1)] focus:bg-[rgba(255,255,255,0.1)] sm:px-6 sm:py-3 px-4 py-3"
+      className="box block rounded-lg text-white font-mplus font-bold text-center hover:bg-[rgba(255,255,255,0.1)] hover:scale-105 focus:bg-[rgba(255,255,255,0.1)] sm:px-6 sm:py-3 px-4 py-3 transition-all duration-300"
       href={href}
       target="_blank"
       rel="noopener noreferrer"
