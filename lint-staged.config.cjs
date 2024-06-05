cbf58762877567221d9798146085fd0480ed1796
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-06-05 13:18:28 +0700

    chore: update lint-staged and next-config

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,21 +1,4 @@
-const { existsSync } = require('fs')
-const { join } = require('path')
-
-const isBun = existsSync(join(process.cwd(), 'bun.lockb'))
-const isYarn = existsSync(join(process.cwd(), 'yarn.lock'))
-const isPnpm = existsSync(join(process.cwd(), 'pnpm-lock.yaml'))
-
-const packageManager = isBun ? 'bun' : isYarn ? 'yarn' : isPnpm ? 'pnpm' : 'npm'
-
-const options = {
-  '**/*.(ts|tsx)': () => `${packageManager} tsc --noEmit`,
-  '**/*.(ts|tsx|js)': filenames => [
-    `${packageManager} eslint --fix ${filenames.join(' ')}`,
-    `${packageManager} next lint ${filenames.join(' ')}`,
-    `${packageManager} prettier --write ${filenames.join(' ')}`
-  ],
-  '**/*.(md|json)': filenames =>
-    `${packageManager} prettier --write ${filenames.join(' ')}`
+module.exports = {
+  '**/*.(ts)': () => `yarn tsc --noEmit`,
+  '**/*.(ts|js|cjs)': () => [`yarn run lint`, `yarn run prettier`]
 }
-
-module.exports = options
