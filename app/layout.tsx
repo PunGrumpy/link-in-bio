706a48326ed8bc3a0c53fc38abf28bb3e17854d0
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-06-12 14:47:26 +0700

    fix(metadata): use `MetaData` type

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,35 +7,42 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Link in Bio',
   description:
-    'Link in Bio is a simple, customizable, and easy-to-use link-in-bio tool for creators.'
+    'Link in Bio is a simple, customizable, and easy-to-use link-in-bio tool for creators.',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/apple-touch-icon.png'
+  },
+  keywords: ['Link in Bio', 'Instagram', 'Linktree'],
+  themeColor: '#000000',
+  authors: {
+    name: 'Noppakorn Kaewsalabnil',
+    url: 'https://pungrumpy.com'
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@pungrumpy_p',
+    creator: '@pungrumpy_p',
+    title: 'Link in Bio',
+    images: '/background.jpg'
+  },
+  openGraph: {
+    title: 'Link in Bio',
+    siteName: 'Link in Bio',
+    type: 'website',
+    url: 'https://bio.pungrumpy.com',
+    images: '/background.jpg',
+    countryName: 'Thailand'
+  }
 }
 
 export default function RootLayout({
   children
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="theme-color" content="#000000" />
-        <meta name="author" content="Noppakorn Kaewsalabnil" />
-        <meta name="keywords" content="Link in Bio, Instagram, Linktree" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:title" content="Link in Bio" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://bio.pungrumpy.com" />
-        <meta property="og:image" content="/background.jpg" />
-        <meta property="og:site_name" content="Link in Bio" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@pungrumpy_p" />
-        <meta name="twitter:creator" content="@pungrumpy_p" />
-        <meta name="twitter:title" content="Link in Bio" />
-        <meta name="twitter:image" content="/background.jpg" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
