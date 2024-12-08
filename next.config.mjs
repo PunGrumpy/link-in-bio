1a5dfa62195e73684fae06c7fc9d5401ad107979
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-12-09 03:14:26 +0700

    feat: add environment variables for LogLib and metadata configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_LOGLIB_ID: process.env.NEXT_PUBLIC_LOGLIB_ID,
+    NEXT_PUBLIC_METADATA_BASE: process.env.NEXT_PUBLIC_METADATA_BASE
+  }
+}
 
 export default nextConfig
