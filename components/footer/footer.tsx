0a2af7c293f96855d3b9851a46207fb07e1a18b2
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-02-11 15:53:19 +0700

    refactor(footer): simplify footer layout and improve responsiveness

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -12,13 +12,13 @@ export function Footer({ url, className }: FooterProps) {
   return (
     <footer
       className={cn(
-        'text-foreground/500 absolute bottom-5 left-1/2 z-10 w-full min-w-max -translate-x-1/2 px-20 text-center text-sm sm:min-w-0 sm:max-w-4xl',
+        'text-foreground/500 absolute bottom-5 left-1/2 z-10 w-full -translate-x-1/2 px-20 text-center text-sm',
         className
       )}
     >
-      <div className="grid items-center justify-items-center gap-4 sm:grid-cols-3">
+      <div className="grid items-center justify-items-center gap-4 sm:grid-cols-2 sm:justify-between sm:justify-items-stretch">
         <Status />
-        <div className="items-center sm:justify-center">
+        <div className="items-center text-center sm:justify-center sm:text-end">
           &copy; {new Date().getUTCFullYear()}&nbsp;
           <Link
             href={url}
@@ -27,29 +27,6 @@ export function Footer({ url, className }: FooterProps) {
             Noppakorn Kaewsalabnil.
           </Link>
           &nbsp; All rights reserved.
-        </div>
-        <div className="items-center sm:justify-center">
-          Built with&nbsp;
-          <Link
-            href="https://nextjs.org"
-            className="border-b border-transparent text-[#ff4500] transition-all duration-500 hover:border-[#ff4500]"
-          >
-            Next.js
-          </Link>
-          &nbsp;+&nbsp;
-          <Link
-            href="https://tailwindcss.com"
-            className="border-b border-transparent text-[#38b2ac] transition-all duration-500 hover:border-[#38b2ac]"
-          >
-            Tailwind CSS
-          </Link>
-          &nbsp;+&nbsp;
-          <Link
-            href="https://vercel.com"
-            className="border-b border-transparent text-white transition-all duration-500 hover:border-white"
-          >
-            Vercel
-          </Link>
         </div>
       </div>
     </footer>
