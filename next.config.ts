d8f73c077d92518104f4ac6d73c2eb7830fa04dd
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-10-02 20:59:26 +0700

    refactor(next.config): simplify configuration by removing experimental settings and environment variables

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
-import { NextConfig } from 'next'
+import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  experimental: {
-    optimizeCss: true,
-    optimizePackageImports: ['@radix-ui/react-icons']
-  },
-  env: {
-    NEXT_PUBLIC_LOGLIB_ID: process.env.NEXT_PUBLIC_LOGLIB_ID,
-    NEXT_PUBLIC_METADATA_BASE: process.env.NEXT_PUBLIC_METADATA_BASE
-  }
-}
+const nextConfig: NextConfig = {}
+
+export default nextConfig
