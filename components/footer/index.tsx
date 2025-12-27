fde1515328277d68216492a4a0186a2cbbcd414c
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-12-27 23:29:52 +0700

    fix(footer): update footer link to use a direct URL instead of a variable for improved clarity

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link'
 import { Status } from '@/components/footer/status'
 
-const url = 'https://pungrumpy.com'
-
 export const Footer = () => (
   <footer className="absolute bottom-5 left-1/2 z-10 w-full -translate-x-1/2 px-20 text-center text-foreground/500 text-sm">
     <div className="grid items-center justify-items-center gap-4 sm:grid-cols-2 sm:justify-between sm:justify-items-stretch">
@@ -11,7 +9,7 @@ export const Footer = () => (
         &copy; {new Date().getUTCFullYear()}&nbsp;
         <Link
           className="border-transparent border-b text-[#ff4500] transition-all duration-500 hover:border-[#ff4500]"
-          href={url}
+          href="https://pungrumpy.com"
         >
           Noppakorn Kaewsalabnil.
         </Link>
